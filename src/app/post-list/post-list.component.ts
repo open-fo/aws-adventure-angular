a3941899755945f4e495e438f312f57fba9c38dd
cbinckly <cbinckly@gmail.com>
2019-02-01 17:20:33 +0100

    update the post list to retrieve posts using the post service and render the titles.

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
 
 @Component({
-  selector: 'app-post-list',
-  templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.scss']
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.scss']
 })
 export class PostListComponent implements OnInit {
 
-  constructor() { }
+    public posts;
 
-  ngOnInit() {
-  }
+    constructor(private _postService: PostService) { }
 
+    ngOnInit() {
+        this.getPosts();
+    }
+
+    private getPosts(): void {
+        this._postService.getPosts().subscribe(
+            data => {
+                this.posts = data;
+            },
+            error => {
+                console.error(error);
+            },
+            () => {
+                console.log("Loaded posts.");
+            }
+        )
+    }
 }
