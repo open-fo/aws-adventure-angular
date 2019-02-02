45066dc17cf01c67944d45560c7641b1b7ca919d
cbinckly <cbinckly@gmail.com>
2019-02-02 12:25:23 +0100

    connect post component to /posts/:id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PostListComponent } from './post-list/post-list.component';
+import { PostComponent } from './post/post.component';
 
 const routes: Routes = [
     {path: '', component: PostListComponent},
+    {path: 'posts/:id', component: PostComponent},
 ];
 
 @NgModule({
